6c874d1b5f77bf2d3c7da853d90fcc55be03fe0a
Mario Gutierrez <mariosubspace@gmail.com>
2021-10-16 23:26:30 -0500

    .

--- httpsserver.py
+++ httpsserver.py
@@ -1,12 +1,15 @@
+# Basic HTTPS server for local testing.
+# Originally from: https://stackoverflow.com/questions/19705785/python-3-simple-https-server
+
+# Generate a certificate with the following command:
 # openssl req -new -x509 -keyout localhost.pem -out localhost.pem -days 365 -nodes
+#
 # You need to specify keyfile if the certificate does not contain the private key
 # https://docs.python.org/3/library/ssl.html#combined-key-and-certificate
 
-# from: https://stackoverflow.com/questions/19705785/python-3-simple-https-server
-
 import http.server, ssl
 
-print("Simple HTTPS server")
+print("Running HTTPS server.")
 
 server_address = ('0.0.0.0', 443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
